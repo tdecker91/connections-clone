170d432bf256376f9b93ee6d16994a3ba4bc89ea
Tyson Decker <tyson.decker@gmail.com>
2025-08-01 22:26:30 -0600

    update base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/connections/',
+  base: '/connections-clone/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
